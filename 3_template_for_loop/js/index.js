ab8cad342c98cc0ba786d5aa1615ef4706f98ce2
Noura Kaddoura <nourakaddoura@gmail.com>
2019-10-09 17:49:21 -0700

    Class-16-objects-loops-this

---
+++ 3_template_for_loop/js/index.js
@@ -0,0 +1,34 @@
+var products    = ['apples', 'oranges', 'bananas', 'kiwi', 'carrots', 'grapes'];
+var details     = [
+    'This is a delicious apple',
+    'I love oranges! They are the best.',
+    'Bananas are the safest fruit to eat unorganicly.',
+    'Why do kiwis have hair?',
+    'Carrots are the best'
+];
+var images      = ['apples.png'];
+var prices      = [1.25, 1.00, .19, .75, .50, 2.00];
+var inventories = [100, 15, 30, 500, 100, 2];
+var cart        = [0, 0, 0, 0, 0];
+var total       = 0;
+
+$(function(){
+    //Add products to the store
+    
+
+    //Add product event
+  
+
+});
+
+//build the select how many
+function buildSelect() 
+{
+ 
+}
+
+//change a number to a currency string
+function currencyFormat()
+{ 
+
+}
